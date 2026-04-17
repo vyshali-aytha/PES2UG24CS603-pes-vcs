813696f3f9cd7a47e0033a02774e6052d85215db
vyshali-aytha <vyshaliaytha@gmail.com>
2026-04-17 16:19:32 +0530

    Phase 4: added commit metadata including author, timestamp, and message

--- commit.c
+++ commit.c
@@ -204,5 +204,7 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 } else {
     c.has_parent = 0;
 }
-
+snprintf(c.author, sizeof(c.author), "%s", pes_author());
+c.timestamp = (uint64_t)time(NULL);
+snprintf(c.message, sizeof(c.message), "%s", message);
 }
